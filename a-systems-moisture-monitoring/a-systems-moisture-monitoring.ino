fdb502c58af63a720adc1ab87fd645e96b69f1ae
Niklas-Mezynski <n.mezynski@student.fontys.nl>
2023-05-15 20:15:39 +0200

    Argus

--- a-systems-moisture-monitoring/a-systems-moisture-monitoring.ino
+++ a-systems-moisture-monitoring/a-systems-moisture-monitoring.ino
@@ -14,23 +14,129 @@
 #include <Wire.h>
 
 // Comment in/out for toggling debug mode (Serial prints and so on)
-// #define DEBUG
+#define DEBUG
 
 // WiFi Connection
 const char *ssid = "MagentaWLAN-Mezynski";
 const char *password = "MANS4mezy";
 
 // HTTP definitions
-const char *url = "https://webserver-a-systems-individual-production.up.railway.app/trpc/sensors.addSensorData";
-// const char *url = "http://192.168.2.52:3002/trpc/sensors.addSensorData"; // For TESTING!
+// const char *url = "https://webserver-a-systems-individual-production.up.railway.app/trpc/sensors.addSensorData";
+const char *url = "http://192.168.2.52:3002/trpc/sensors.addSensorData"; // For TESTING!
 const char *apiAuthorization = "E3o8zuQp3W2izyvmBK0DqGhFecWtEqEa";
 
 // Sensor definitions (General)
-const int MILLIS_BETWEEN_READINGS = 3000;       // 3 Seconds
-const int MILLIS_BETWEEN_DATA_SENDING = 300000; // 5 Minutes
-// const int MILLIS_BETWEEN_READINGS = 1000;      // 1 Seconds - For TESTING!
-// const int MILLIS_BETWEEN_DATA_SENDING = 60000; // 1 Minute - For TESTING!
+// const int MILLIS_BETWEEN_READINGS = 3000;       // 3 Seconds
+// const int MILLIS_BETWEEN_DATA_SENDING = 300000; // 5 Minutes
+const int MILLIS_BETWEEN_READINGS = 1000;      // 1 Seconds - For TESTING!
+const int MILLIS_BETWEEN_DATA_SENDING = 60000; // 1 Minute - For TESTING!
 const int NUM_READINGS = MILLIS_BETWEEN_DATA_SENDING / MILLIS_BETWEEN_READINGS;
+
+// Class or struct "Sensor" that contains the sensor data and the sensor type
+class Sensor
+{
+private:
+  int idx;
+
+public:
+  uint8_t pin;
+  String sensorName;
+  int readings[NUM_READINGS] = {0};
+  int readingsPerc[NUM_READINGS] = {0};
+  int total;
+  int totalPerc;
+  int minADC;
+  int maxADC;
+  Sensor(uint8_t pin, String sensorName, int minADC, int maxADC)
+  {
+    this->pin = pin;
+    this->sensorName = sensorName;
+    this->minADC = minADC;
+    this->maxADC = maxADC;
+    this->total = 0;
+    this->totalPerc = 0;
+    this->idx = 0;
+  }
+
+  // Function that reads the sensor value
+  int readSensor()
+  {
+    int rawValue = analogRead(this->pin);
+    this->total = this->total - this->readings[this->idx] + rawValue;
+    this->readings[this->idx] = rawValue;
+
+    int percValue = map(rawValue, this->minADC, this->maxADC, 0, 100);
+
+    this->totalPerc = this->totalPerc - this->readingsPerc[this->idx] + percValue;
+    this->readingsPerc[this->idx] = percValue;
+
+    this->idx = (this->idx + 1) % NUM_READINGS;
+
+    return rawValue;
+  }
+
+  // Function that calculates the rolling average
+  float getRollingAverage()
+  {
+
+    return this->total / (float)NUM_READINGS;
+  }
+
+  // Function that calculates the percentage
+  float getPercentageAverage()
+  {
+    return this->totalPerc / (float)NUM_READINGS;
+  }
+
+  void postSensorData()
+  {
+    float averageRaw = this->getRollingAverage();
+    float averagePerc = this->getPercentageAverage();
+
+    // Create a JSON document
+    StaticJsonDocument<200> doc;
+    doc["rawValue"] = static_cast<int>(averageRaw);
+    doc["humidity"] = averagePerc;
+    doc["sensorType"] = this->sensorName;
+
+    // Serialize the JSON document to a string
+    String json;
+    serializeJson(doc, json);
+
+    // Send an HTTP POST request to the REST API
+    HTTPClient http;
+    http.begin(url);
+    http.addHeader("Content-Type", "application/json");
+    http.addHeader("Authorization", apiAuthorization);
+    int httpCode = http.POST(json);
+#ifdef DEBUG
+    if (httpCode > 0)
+    {
+      Serial.printf("[HTTP] POST request sent, status code: %d\n", httpCode);
+      String response = http.getString();
+      Serial.println(response);
+    }
+    else
+    {
+      Serial.printf("[HTTP] POST request failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+#endif
+    http.end();
+  }
+
+  void printSensorData()
+  {
+#ifdef DEBUG
+    float avg = this->getRollingAverage();
+    float averagePerc = this->getPercentageAverage();
+
+    int lastReading = this->readings[(this->idx + NUM_READINGS - 1) % NUM_READINGS];
+    int percValue = map(lastReading, this->minADC, this->maxADC, 0, 100);
+
+    Serial.printf("%s Sensor -- Moisture = %d\% [Avg: %.2f\%] | (Raw: %d [Avg: %.2f])\n", this->sensorName, percValue, averagePerc, lastReading, avg);
+#endif
+  }
+};
 
 // Sensor definitions (Main Sensor)
 
@@ -41,33 +147,25 @@ const int NUM_READINGS = MILLIS_BETWEEN_DATA_SENDING / MILLIS_BETWEEN_READINGS;
  * Green (GND) -> GND (GND)
  */
 
-#define AOUT_PIN 36 // Analog input pin that the sensor output is attached to(white wire)
+// For ADC -> use pins (GPIO 0, 2, 4, 12-15, 25-27, 32-39)
 
-bool is1V1Output = false; // set true if 1.1V output sensor is used for 3V set to false
-
-float refVoltage = 3.3; // set reference voltage - by default Arduino supply voltage (UNO - 5V)
-
-int minADC = 0;    // replace with min ADC value read in air
-int maxADC = 3500; // replace with max ADC value read fully submerged in water
-
-int rawValue, mappedValue;
-
-int readings[NUM_READINGS];
-int readingsPerc[NUM_READINGS];
-int idx = 0;
-int total = 0;
-int totalPerc = 0;
-
-const char *sensorNameMain = "main";
-float averageRawMain = 0;
-float averagePercMain = 0;
+std::vector<Sensor> sensors;
 
 void setup()
 {
 #ifdef DEBUG
   Serial.begin(115200);
 #endif
+
+  // Initialize the sensors
+  // Sensor s1 = Sensor(36, "main", 0, 3500);
+  Sensor s2 = Sensor(36, "second", 4095, 2045);
+
+  sensors.push_back(s2);
+
   connectWiFi();
+
+  // Initialize the sensor readings to 0
 
 #ifdef DEBUG
   Serial.println("Setup done!");
@@ -76,11 +174,13 @@ void setup()
 
 void loop()
 {
-  readSensorData();
-  // readSecondSensorData();
+  for (auto &sensor : sensors)
+  {
+    sensor.readSensor();
 #ifdef DEBUG
-  printSensorData();
+    sensor.printSensorData();
 #endif
+  }
 
   sendDataToServer();
 
@@ -96,62 +196,12 @@ void sendDataToServer()
   // check if 5 minutes have passed since the last action
   if (currentTime - lastTime >= MILLIS_BETWEEN_DATA_SENDING)
   {
-    performHTTP_POST_Request();
+    for (auto &sensor : sensors)
+    {
+      sensor.postSensorData();
+    }
     lastTime = currentTime;
   }
-}
-
-void readSensorData()
-{
-  rawValue = analogRead(AOUT_PIN); // read the analog value from sensor
-
-  mappedValue = map(rawValue, minADC, maxADC, 0, 100);
-
-  // Add the reading to the rolling average
-  total = total - readings[idx] + rawValue;
-  readings[idx] = rawValue;
-
-  totalPerc = totalPerc - readingsPerc[idx] + mappedValue;
-  readingsPerc[idx] = mappedValue;
-
-  idx = (idx + 1) % NUM_READINGS;
-
-  // Calculate the rolling average
-  averageRawMain = total / (float)NUM_READINGS;
-  averagePercMain = totalPerc / (float)NUM_READINGS;
-}
-
-void performHTTP_POST_Request()
-{
-  // Create a JSON document
-  StaticJsonDocument<200> doc;
-  doc["rawValue"] = static_cast<int>(averageRawMain);
-  doc["humidity"] = averagePercMain;
-  doc["sensorType"] = sensorNameMain;
-
-  // Serialize the JSON document to a string
-  String json;
-  serializeJson(doc, json);
-
-  // Send an HTTP POST request to the REST API
-  HTTPClient http;
-  http.begin(url);
-  http.addHeader("Content-Type", "application/json");
-  http.addHeader("Authorization", apiAuthorization);
-  int httpCode = http.POST(json);
-#ifdef DEBUG
-  if (httpCode > 0)
-  {
-    Serial.printf("[HTTP] POST request sent, status code: %d\n", httpCode);
-    String response = http.getString();
-    Serial.println(response);
-  }
-  else
-  {
-    Serial.printf("[HTTP] POST request failed, error: %s\n", http.errorToString(httpCode).c_str());
-  }
-#endif
-  http.end();
 }
 
 void connectWiFi()
@@ -180,7 +230,7 @@ void connectWiFi()
 #endif
 }
 
-// -- SECOND SENSOR STUFF --
+// -- SECOND SENSOR STUFF (deprecated) --
 
 /**
  * -- ESP32 connections --
@@ -252,11 +302,4 @@ unsigned int readI2CRegister16bit(int addr, int reg)
   unsigned int t = Wire.read() << 8;
   t = t | Wire.read();
   return t;
-}
-
-void printSensorData()
-{
-  Serial.printf("Main Sensor -- Moisture = %d\% [Avg: %.3f\%] | (Raw: %d [Avg: %.3f])", mappedValue, averagePercMain, rawValue, averageRawMain);
-  Serial.printf(" - ");
-  Serial.printf("2nd Sensor -- Moisture = %d\% [Avg: %.3f\%] | (Raw: %d [Avg: %.3f])\n", mappedValue2nd, averagePerc2nd, rawValue2nd, averageRaw2nd);
 }
